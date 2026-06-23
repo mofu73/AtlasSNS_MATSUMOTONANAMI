f46f638324a8e29ea4f2a551dbe77b99899a4b38
DESKTOP-8J41R10\NANAMI <matumotoseven@gmail.com>
2026-06-24 01:00:03 +0900

    赤ボタン

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -7,6 +7,7 @@
     <p class="finish">ユーザー登録が完了しました。</p>
     <p class="news">早速ログインをしてみましょう。</p>
 
-    {{ Form::submit('ログイン画面へ',['class' => 'btn btn-danger']) }}
+    <p class="login_back5"><a href="/login">ログイン画面へ</a></p>
+
   </div>
 </x-logout-layout>
